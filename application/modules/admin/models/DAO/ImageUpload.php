f035e6cc109868ff48f8f7de35d055c227021a6f
binh12121989@gmail.com <binh12121989@gmail.com@c8020842-7d4b-62ec-05bb-019919046af6>
2013-08-04 09:46:14 +0000

    - Image upload management

--- application/modules/admin/models/DAO/ImageUpload.php
+++ application/modules/admin/models/DAO/ImageUpload.php
@@ -31,4 +31,65 @@ class Admin_Model_DAO_ImageUpload
        $result = $this->_db->lastInsertId('user_upload', 'id');
        return $result;
    }
+   
+   /**
+    * Get image upload by given options
+    * @param array $options
+    * @return array
+    * */
+   public function getImageUploads($options) {
+       $sql = "SELECT *  ";
+       $from = " FROM `user_upload` ";
+       $where = " WHERE 1 = 1 ";
+       if($options['title']) {
+           $where .= " AND `title` LIKE '%{$options['title']}%' ";    
+       }
+       if($options['category']) {
+           $where .= " AND `category` = {$this->_db->quote($options['category'])} ";
+       }
+       if($options['username']) {
+           $where .= " AND `username` = {$this->_db->quote($options['username'])} ";    
+       }
+       if($options['usernames']) {
+           $where .= " AND `username` IN ({$options['usernames']}) ";
+       }
+       
+       $order = " ORDER BY `user_upload`.`id` DESC ";
+       if($options['order'] && $options['by']) {
+       	    $order = " ORDER BY `user_upload`.`{$options['order']}` {$options['by']} ";
+       }
+       
+       $limit = "";
+       if(isset($options['offset']) && $options['offset'] >=0 && $options['limit'] >0) {
+       	$limit = " limit {$this->_db->quote($options['offset'], 'INTEGER')}, {$this->_db->quote($options['limit'], 'INTEGER')} ";
+       }
+       $result = $this->_db->fetchAll ( $sql . $from . $where . $order . $limit );
+       return $result;
+   }
+   
+   /**
+    * Get image upload by given options
+    * @param array $options
+    * @return integer
+    * */
+   public function getImageUploadsCount($options) {
+       $sql = "SELECT count(*) as `count`  ";
+       $from = " FROM `user_upload` ";
+       $where = " WHERE 1 = 1 ";
+       if($options['title']) {
+       	$where .= " AND `title` = '%{$options['title']}%' ";
+       }
+       if($options['category']) {
+       	$where .= " AND `category` = {$this->_db->quote($options['category'])} ";
+       }
+       if($options['username']) {
+       	$where .= " AND `username` = {$this->_db->quote($options['username'])} ";
+       }
+       if($options['usernames']) {
+       	$where .= " AND `username` IN ({$options['usernames']}) ";
+       }
+        
+   	    $result = $this->_db->fetchOne ( $sql . $from . $where);
+       return $result;
+   }
 }
